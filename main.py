23457285d30de02c276a856d4b852b331696854d
adantop <>
2021-02-24 09:42:24 -0600

    Added Procfile

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect
 from resources import db, SportsGood
 
 app = Flask(__name__)
